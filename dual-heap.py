0f3288a2377a2b65caeb165f9b8c4d620263e8ed
ifthikhan <iftecan2000@gmail.com>
2014-02-09 23:23:20 +0000

    Added dual heap initial version with tests.

---
+++ dual-heap.py
@@ -0,0 +1,138 @@
+"""
+Create a binary heap for min and max values of inputs with the following
+interface
+    - add
+    - delete
+    - get
+    - size
+"""
+
+import unittest
+
+class BinaryHeap(object):
+
+    def __init__(self):
+        self._vals = []
+
+    def __len__(self):
+        return len(self._vals)
+
+    def add(self, v):
+        self._vals.append(v)
+        self._swim()
+
+    def get(self):
+        last = len(self) - 1
+        self._exch(0, last)
+        ret_val = self._vals.pop(last)
+        self._sink()
+        return ret_val
+
+    def peek(self):
+        return self._vals[0]
+
+    def _sink(self):
+        parent_ix = 0
+        left_ix, right_ix = self._get_children_ixs(parent_ix)
+        while left_ix or right_ix:
+            if left_ix and right_ix:
+                if self._vals[left_ix] > self._vals[right_ix]:
+                    to_exch = left_ix
+                else:
+                    to_exch = right_ix
+            elif left_ix:
+                to_exch = left_ix
+            elif right_ix:
+                to_exch = right_ix
+
+            self._exch(parent_ix, to_exch)
+            parent_ix = to_exch
+            left_ix, right_ix = self._get_children_ixs(parent_ix)
+
+    def _swim(self):
+        child_ix = len(self) - 1
+        if child_ix == 0:
+            return
+        parent_ix = self._get_parent(child_ix)
+        while (self._test(parent_ix, child_ix)):
+            self._exch(parent_ix, child_ix)
+            #TODO: UNGLY
+            if parent_ix == 0:
+                break
+            child_ix = parent_ix
+            parent_ix = self._get_parent(parent_ix)
+
+    def _exch(self, ix_1, ix_2):
+        tmp = self._vals[ix_1]
+        self._vals[ix_1] = self._vals[ix_2]
+        self._vals[ix_2] = tmp
+
+    def _get_parent(self, child_ix):
+        t = 1 if child_ix & 1 else 2
+        return (child_ix - t) / 2
+
+    def _get_children_ixs(self, parent_ix):
+        ix_left = (parent_ix * 2) + 1
+        ix_right = ix_left + 1
+        if ix_left >= len(self):
+            ix_left, ix_right = None, None
+        elif ix_right > len(self) - 1:
+            ix_right = None
+        return ix_left, ix_right
+
+    def _test(self, parent_ix, child_ix):
+        return self._vals[child_ix] > self._vals[parent_ix]
+
+
+class BinaryHeapTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.h = BinaryHeap()
+
+    def test_add_initialization(self):
+        self.assertEqual(len(self.h), 0)
+
+    def test_add(self):
+        self.h.add(1)
+        self.assertEqual(len(self.h), 1)
+
+    def test_get(self):
+        self.h.add(2)
+        self.h.add(1)
+        self.assertEqual(2, self.h.get())
+        self.assertEqual(len(self.h), 1)
+
+    def test_peek(self):
+        self.h.add(1)
+        self.assertEqual(1, self.h.peek())
+        self.assertEqual(len(self.h), 1)
+
+    def test_heap_value_top(self):
+        self.h.add(1)
+        self.h.add(2)
+        self.assertEqual(2, self.h.get())
+
+    def test_heapify(self):
+        self.h.add(1)
+        self.h.add(2)
+        self.assertEqual(2, self.h.peek())
+        self.h.add(10)
+        self.assertEqual(10, self.h.peek())
+        self.h.add(20)
+        self.assertEqual(20, self.h.peek())
+        self.h.add(12)
+        self.assertEqual(20, self.h.peek())
+
+    def test_heapify_after_get(self):
+        self.h.add(7)
+        self.h.add(5)
+        self.h.add(6)
+        self.h.add(1)
+        self.assertEqual(7, self.h.get())
+        self.assertEqual(6, self.h.get())
+        self.assertEqual(5, self.h.get())
+
+
+if __name__ == "__main__":
+    unittest.main()
+
